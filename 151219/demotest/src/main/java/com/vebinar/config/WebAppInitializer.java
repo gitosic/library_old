0a027bb6eff29ea5cab5801d0a852a90bd2fccf5
Vitaliy Kosyuk <vitaliu2000@live.com>
2019-12-27 15:40:22 +0400

    #1

---
+++ 151219/demotest/src/main/java/com/vebinar/config/WebAppInitializer.java
@@ -0,0 +1,29 @@
+package com.vebinar.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+
+//                                  https://youtu.be/DclLJt8zDWk?t=1716
+//Класс будет при старте создавать апликейшн контекст
+//Создадим диспачер сервлет
+//имплементим WebApplicationInitializer
+//одим метод onStartup, который будет запускаться при старте приложения
+//Это у нас поднимается спринг контекст и загрузились все депенденси которые мы опишем в этоим конфиг фвайле.
+public class WebAppInitializer /*implements WebApplicationInitializer*/ {
+    /*@Override
+    public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
+
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(SpringConfig.class, WebConfig.class);//регистрируем классы с конфигами
+        context.setServletContext(servletContext);//прописываем servletContext, поскольку он приходит в этот класс
+
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(context));//создадим диспачер сервлет
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");//]обрабатывает все начиная с мамого начала.
+    }*/
+}
---
+++ 151219/demotest/src/main/java/com/vebinar/controller/UserController.java
@@ -0,0 +1,5 @@
+package com.vebinar.controller;
+
+//Будет работ с веб прил, будет обрабатывать нащи запросы с темплейтом
+public class UserController {
+}
